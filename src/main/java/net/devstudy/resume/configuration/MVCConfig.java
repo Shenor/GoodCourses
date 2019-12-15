a6c401c4a4fb269558012d18a48e17d2e54945b8
a.osetckiy <a.osetckiy@extracode.net>
2019-12-15 13:56:57 +0300

    ORM, JPA, Spring Data: added entities

--- src/main/java/net/devstudy/resume/configuration/MVCConfig.java
+++ src/main/java/net/devstudy/resume/configuration/MVCConfig.java
@@ -3,6 +3,7 @@ package net.devstudy.resume.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -12,7 +13,7 @@ import org.springframework.web.servlet.view.JstlView;
 
 /**
  * https://www.luckyryan.com/2013/02/07/migrate-spring-mvc-servlet-xml-to-java-config/
- * 
+ *
  * @author devstudy
  * @see http://devstudy.net
  */
@@ -20,7 +21,7 @@ import org.springframework.web.servlet.view.JstlView;
 @EnableWebMvc
 @ComponentScan({ "net.devstudy.resume.controller" })
 public class MVCConfig extends WebMvcConfigurerAdapter {
-	
+
 	@Bean
 	public ViewResolver viewResolver() {
 		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
@@ -35,5 +36,11 @@ public class MVCConfig extends WebMvcConfigurerAdapter {
 		registry.addResourceHandler("/static/**").addResourceLocations("/static/");
 		registry.addResourceHandler("/media/**").addResourceLocations("/media/");
 		registry.addResourceHandler("/favicon.ico").addResourceLocations("/favicon.ico");
+	}
+
+	@Bean
+	public CommonsMultipartResolver multipartResolver(){
+		CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+		return multipartResolver;
 	}
 }
