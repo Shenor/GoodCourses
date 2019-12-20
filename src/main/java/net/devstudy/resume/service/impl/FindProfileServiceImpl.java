6b8ea0136d44cf5e60d8db8d5471cc3af7d0615e
a.osetckiy <a.osetckiy@extracode.net>
2019-12-20 17:12:50 +0300

    The basics of ajax loading with an example Load more

--- src/main/java/net/devstudy/resume/service/impl/FindProfileServiceImpl.java
+++ src/main/java/net/devstudy/resume/service/impl/FindProfileServiceImpl.java
@@ -1,6 +1,8 @@
 package net.devstudy.resume.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import net.devstudy.resume.entity.Profile;
@@ -12,9 +14,14 @@ public class FindProfileServiceImpl implements FindProfileService {
 
 	@Autowired
 	private ProfileRepository profileRepository;
-	
+
 	@Override
 	public Profile findByUid(String uid) {
 		return profileRepository.findByUid(uid);
+	}
+
+	@Override
+	public Page<Profile> findAll(Pageable pageable) {
+		return profileRepository.findAll(pageable);
 	}
 }
